95b8573f37467081554caf242691659caed93a73
Pietro <pietro.varraschim@hotmail.com>
2025-08-28 18:32:19 -0300

    Ajustei a cor do tipo ???
    E também comecei testes do dexnav

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -659,11 +659,11 @@
 #define FLAG_EXP_SHARE  0x264 // EXP SHARE
 #define FLAG_TERA_ORB_CHARGED  0x265 // TERA
 #define FLAG_TERA_ORB_NO_COST  0x266 // Unused Flag
-#define FLAG_UNUSED_0x267  0x267 // Unused Flag
-#define FLAG_UNUSED_0x268  0x268 // Unused Flag
-#define FLAG_UNUSED_0x269  0x269 // Unused Flag
-#define FLAG_UNUSED_0x26A  0x26A // Unused Flag
-#define FLAG_UNUSED_0x26B  0x26B // Unused Flag
+#define FLAG_TESTE_1  0x267 // Unused Flag
+#define FLAG_TESTE_2  0x268 // Unused Flag
+#define FLAG_TESTE_3  0x269 // Unused Flag
+#define FLAG_TESTE_4  0x26A // Unused Flag
+#define FLAG_TESTE_5  0x26B // Unused Flag
 #define FLAG_UNUSED_0x26C  0x26C // Unused Flag
 #define FLAG_UNUSED_0x26D  0x26D // Unused Flag
 #define FLAG_UNUSED_0x26E  0x26E // Unused Flag
